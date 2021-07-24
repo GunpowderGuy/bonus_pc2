10aaab20254e9a3c0f11e89e4ff57f72ee1bb8ad
GunpowderGuy <diegorosario2013@gmail.com>
2021-07-24 17:25:45 -0500

    heap( ejer 1 )

--- src/p1.h
+++ src/p1.h
@@ -2,11 +2,28 @@
 // Created by rudri on 10/11/2020.
 //
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 #ifndef POO2_PC2_SEC02_V2021_1_P1_H
 #define POO2_PC2_SEC02_V2021_1_P1_H
-
 
 void question_1_1();
 void question_1_2();
 
-#endif //POO2_PC2_SEC02_V2021_1_P1_H
+template <typename T> using MegaVec = std::vector<std::vector<T>>;
+
+template <typename T> MegaVec<T> join_sort(MegaVec<T> megacont) {
+  MegaVec<T> result;
+
+  for (auto elem : megacont) {
+    result.insert(std::end(result), std::begin(elem), std::end(elem));
+  }
+
+  std::make_heap(begin(result),end(result));
+
+  return result;
+}
+
+#endif // POO2_PC2_SEC02_V2021_1_P1_H
