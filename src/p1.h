234db837932baeff740895a5cc7c9a656ae29ae9
GunpowderGuy <diegorosario2013@gmail.com>
2021-07-24 17:42:25 -0500

    documentacion

--- src/p1.h
+++ src/p1.h
@@ -2,9 +2,9 @@
 // Created by rudri on 10/11/2020.
 //
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 #ifndef POO2_PC2_SEC02_V2021_1_P1_H
 #define POO2_PC2_SEC02_V2021_1_P1_H
@@ -18,10 +18,11 @@ template <typename T> MegaVec<T> join_sort(MegaVec<T> megacont) {
   MegaVec<T> result;
 
   for (auto elem : megacont) {
+    // https://stackoverflow.com/questions/201718/concatenating-two-stdvectors
     result.insert(std::end(result), std::begin(elem), std::end(elem));
   }
-
-  std::make_heap(begin(result),end(result));
+  // https://stackoverflow.com/questions/947394/what-is-the-point-of-make-heap
+  std::make_heap(begin(result), end(result));
 
   return result;
 }
--- src/p3.h
+++ src/p3.h
@@ -9,4 +9,7 @@
 void question_3_1();
 void question_3_2();
 
+//https://www.geeksforgeeks.org/find-distance-between-two-nodes-of-a-binary-tree/
+//https://stackoverflow.com/questions/2603692/what-is-the-difference-between-tree-depth-and-height
+
 #endif //POO2_PC2_SEC02_V2021_1_P3_H
