f742ee4fb2454f5b4d897af23e5a87b3c310a297
GunpowderGuy <diegorosario2013@gmail.com>
2021-07-25 08:37:04 -0500

    merge tests

--- src/p1.h
+++ src/p1.h
@@ -2,6 +2,7 @@
 // Created by rudri on 10/11/2020.
 //
 
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -9,6 +10,11 @@
 #ifndef POO2_PC2_SEC02_V2021_1_P1_H
 #define POO2_PC2_SEC02_V2021_1_P1_H
 
+template <template<typename ...> class CTemplate, typename CType, typename T = typename CType::value_type>
+CType join_sort(CTemplate<CType> containers) {
+    return CType{};
+
+}
 void question_1_1();
 void question_1_2();
 
@@ -27,4 +33,4 @@ template <typename T> MegaVec<T> join_sort(MegaVec<T> megacont) {
   return result;
 }
 
-#endif // POO2_PC2_SEC02_V2021_1_P1_H
+#endif //POO2_PC2_SEC02_V2021_1_P1_H
